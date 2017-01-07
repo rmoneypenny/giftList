7cd5640b1461d683a7d4e9dadc847d7765c179e3
Randy <r_moneypenny@hotmail.com>
2017-01-06 20:42:49 -0500

    changes made for postgresql

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 gem 'byebug', platform: :mri
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161222010945) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "item_name"
     t.float    "price"
