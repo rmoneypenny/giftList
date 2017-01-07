833dfc7a8c35cd5e528a65109e2b7015218712f2
Randy <r_moneypenny@hotmail.com>
2017-01-06 22:38:14 -0500

    troubleshooting heroku db errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222010945) do
+ActiveRecord::Schema.define(version: 20170107033619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 20161222010945) do
     t.string   "item_name"
     t.float    "price"
     t.string   "link"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "deleted"
+    t.integer  "user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
